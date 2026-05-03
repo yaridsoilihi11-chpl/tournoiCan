7edd3d8fdb1ace44bc2a88961242d974002b6c34
yaridsoilihi11-chpl <yarid.soilihi11@gmail.com>
2026-05-03 17:41:26 +0200

    add captain phone

---
+++ prisma/migrations/20260503154109_add_captain_phone/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `captainPhone` to the `Team` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Team" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "captainPhone" TEXT NOT NULL
+);
+INSERT INTO "new_Team" ("id", "name") SELECT "id", "name" FROM "Team";
+DROP TABLE "Team";
+ALTER TABLE "new_Team" RENAME TO "Team";
+CREATE UNIQUE INDEX "Team_name_key" ON "Team"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
